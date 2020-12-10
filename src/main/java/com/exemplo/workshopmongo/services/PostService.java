1b84cc0b8adfbd4eb9833cc5b0001cf807a1e6c3
mmartinschmidt <mmartin.schmidt@gmail.com>
2020-12-10 17:03:36 -0300

    Consulta simples com @Query

--- src/main/java/com/exemplo/workshopmongo/services/PostService.java
+++ src/main/java/com/exemplo/workshopmongo/services/PostService.java
@@ -21,8 +21,12 @@ public class PostService {
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 	}
 	
+	//public List<Post> findByTitle(String text) {
+	//	return repo.findByTitleContainingIgnoreCase(text);
+	//}
+	
 	public List<Post> findByTitle(String text) {
-		return repo.findByTitleContainingIgnoreCase(text);
+		return repo.searchTitle(text);
 	}
 	
 }
